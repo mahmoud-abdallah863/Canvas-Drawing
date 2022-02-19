6beb8f38a37491d6ff2560a987d666b33a60914c
Mahmoud Abdallah <mabdallah863@gmail.com>
2022-02-19 11:22:35 +0200

    Set CanvasView as contentView for MainActivity. Set CanvasView as full screen

--- app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
+++ app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
@@ -2,10 +2,20 @@ package com.mahmoud.canvasdrawing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+import androidx.core.view.doOnLayout
+import com.mahmoud.canvasdrawing.ui.CanvasView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        val canvasView = CanvasView(this).apply {
+            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+            contentDescription = getString(R.string.canvasContentDescription)
+        }
+        setContentView(canvasView)
+
     }
 }
