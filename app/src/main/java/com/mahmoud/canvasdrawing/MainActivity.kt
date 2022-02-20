29e6d02e4a33b116ff574a986d73e31c767c29ae
Mahmoud Abdallah <mabdallah863@gmail.com>
2022-02-20 11:47:47 +0200

    Created clipped view

--- app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
+++ app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
@@ -5,17 +5,20 @@ import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import androidx.core.view.doOnLayout
 import com.mahmoud.canvasdrawing.ui.CanvasView
+import com.mahmoud.canvasdrawing.ui.ClippedView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
-        val canvasView = CanvasView(this).apply {
-            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
-            contentDescription = getString(R.string.canvasContentDescription)
-        }
-        setContentView(canvasView)
+//        val canvasView = CanvasView(this).apply {
+//            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+//            contentDescription = getString(R.string.canvasContentDescription)
+//        }
+
+        val clippedView = ClippedView(this)
+        setContentView(clippedView)
 
     }
 }
