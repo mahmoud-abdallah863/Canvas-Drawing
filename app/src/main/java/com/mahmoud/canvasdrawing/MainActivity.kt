3e164580b3d7ec391a35c87fac0c0b4febc51a72
Mahmoud Abdallah <mabdallah863@gmail.com>
2022-02-26 10:21:26 +0200

    CanvasView: change drawing paint + implement clear drawing functionality. Add CanvasView with 2 buttons in HomeActivity

--- app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
+++ app/src/main/java/com/mahmoud/canvasdrawing/MainActivity.kt
@@ -1,24 +1,36 @@
 package com.mahmoud.canvasdrawing
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
-import androidx.core.view.doOnLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.mahmoud.canvasdrawing.databinding.ActivityMainBinding
 import com.mahmoud.canvasdrawing.ui.CanvasView
-import com.mahmoud.canvasdrawing.ui.ClippedView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonChangeColor.setOnClickListener {
+            binding.canvasView.changeColor(Color.RED)
+        }
+
+        binding.buttonClear.setOnClickListener {
+            binding.canvasView.clear()
+        }
 
 //        val canvasView = CanvasView(this).apply {
 //            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
 //            contentDescription = getString(R.string.canvasContentDescription)
 //        }
-
-        val clippedView = ClippedView(this)
-        setContentView(clippedView)
+//
+////        val clippedView = ClippedView(this)
+//        setContentView(canvasView)
 
     }
 }
